1052841edc6c3607254d34a04bc4a4b349137412
KarpAnt <antonkarp95@mail.ru>
2022-06-02 19:37:39 +0300

    Second

--- src/SealedСlasses.kt
+++ src/SealedСlasses.kt
@@ -1,24 +1,24 @@
 import java.net.CacheRequest
 
-sealed class Request(){
+sealed class Request{
 
-    class Error1():Request(){}
-    class Error2():Request(){}
-    class Answer():Request(){}
+    class Error1(var e:Int):Request()
+    class Error2(var e:Int):Request()
+    class Answer(var e:Int):Request()
+    class W(var e:Int):Request()
+
+}
+fun get(request: Request){
+    var x=request.hashCode()
+    when(request){
+        is Request.Error1 -> println("Код ошибки: $x Ошибка сервера")
+        is Request.Error2 -> println("Код ошибки: $x Нет подключения к интернету")
+        is Request.Answer -> {println("Ответ")
+            println( Request.Answer(1).e )}
+    }
+
 }
 
-
 fun main(){
-
-
-    fun get(request: Request){
-        var x=request.hashCode()
-        when(request){
-            is Request.Error1 -> println("Код ошибки: $x Ошибка сервера")
-            is Request.Error2 -> println("Код ошибки: $x Нет подключения к интернету")
-            is Request.Answer -> println("Ответ")
-        }
-    }
-get(Request.Error1())
-
+get(Request.Answer(1))
 }
